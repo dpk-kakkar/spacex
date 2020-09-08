9073be26f313bf6740058e72d14dd683d1725ebb
deepak <deepak1990k@gmail.com>
2020-09-08 19:23:44 +0530

    deployment changes for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/assets'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/spacex/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
